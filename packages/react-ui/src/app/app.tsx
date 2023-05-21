2f4d53c5de6e8558d4057926cb7a6e58c44841fd
Umar Mohammed <umar.mohammed@holisticai.com>
2023-05-21 11:16:06 +0100

    add react ui

---
+++ packages/react-ui/src/app/app.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { interpreterCore } from '@ts-interpreter/interpreter-core';
+
+export function App() {
+  return <div>{interpreterCore()}</div>;
+}
+
+export default App;
