8fffe449e035b91132b444138df2e517fef9ee98
Umar Mohammed <umar.mohammed@holisticai.com>
2023-06-10 11:13:40 +0100

    add text area to ng-ui
    Co-authored-by: wadefleming-nz <wadefleming-nz@users.noreply.github.com>

--- packages/interpreter-core/src/lib/interpreter-core.ts
+++ packages/interpreter-core/src/lib/interpreter-core.ts
@@ -1,3 +1,7 @@
 export function interpreterCore(): string {
   return 'interpreter-core';
 }
+
+export function interpret(code: string) {
+  console.log('from interpreter', code);
+}
--- packages/ng-ui/src/app/app.component.ts
+++ packages/ng-ui/src/app/app.component.ts
@@ -1,16 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { interpreterCore } from '@ts-interpreter/interpreter-core';
+import { interpret, interpreterCore } from '@ts-interpreter/interpreter-core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
   selector: 'ts-interpreter-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  imports: [CommonModule],
+  template: `<div>
+    <textarea (keyup)="handleKeyUp($event)" cols="30" rows="10"></textarea>
+    <button
+      *ngIf="inputText$ | async as inputText"
+      (click)="handleSubmit(inputText)"
+    >
+      submit
+    </button>
+  </div>`,
 })
 export class AppComponent {
-  title = 'ng-ui';
-
   interpreterCore = interpreterCore();
+
+  inputText$ = new BehaviorSubject('');
+
+  handleKeyUp(e: KeyboardEvent) {
+    this.inputText$.next((e.target as HTMLTextAreaElement).value);
+  }
+
+  handleSubmit(inputText: string) {
+    interpret(inputText);
+  }
 }
